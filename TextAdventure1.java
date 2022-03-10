f9c7a97af69a310ed26dca187a8a11ba1a1234c9
KuzeyOzturac <69856672+KuzeyOzturac@users.noreply.github.com>
2022-03-10 10:43:43 -0500

    Update TextAdventure1.java

--- TextAdventure1.java
+++ TextAdventure1.java
@@ -3,7 +3,7 @@ package textadventure;
 public class TextAdventure1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("You die, rip! Game over!");
 
 	}
 
